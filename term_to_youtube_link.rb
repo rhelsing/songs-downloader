69a18c1b01e66e8528fe6999a76f9b972ee5f9bc
Ryan Helsing <ryanhelsing@gmail.com>
2014-12-07 12:15:57 -0500

    clean up

--- term_to_youtube_link.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-require 'nokogiri'
-require 'open-uri'
-song = ARGV[0]
-song = song.downcase.tr(" ", "+")
-doc = Nokogiri::HTML(open("https://www.youtube.com/results?search_query=#{song}"))
-#watch?v
-url = doc.css("h3.yt-lockup-title a").first['href']
-puts "http://www.youtube.com#{url}"
